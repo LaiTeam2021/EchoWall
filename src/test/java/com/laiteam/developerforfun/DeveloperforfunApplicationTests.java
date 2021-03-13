571a28675c34f471f069ff4395adf3a8feeea615
Tim <jingweihu.usc@gmail.com>
2021-03-12 17:07:32 -0800

    [Backend] Encrypt Database Password

--- src/test/java/com/laiteam/developerforfun/DeveloperforfunApplicationTests.java
+++ src/test/java/com/laiteam/developerforfun/DeveloperforfunApplicationTests.java
@@ -1,13 +1,23 @@
 package com.laiteam.developerforfun;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DeveloperforfunApplication.class)
 class DeveloperforfunApplicationTests {
 
+    @Autowired
+    private StringEncryptor encryptor;
+
     @Test
-    void contextLoads() {
+    public void testJasypt() {
+        System.out.println("加密 = " + encryptor.encrypt("test"));
     }
 
 }
