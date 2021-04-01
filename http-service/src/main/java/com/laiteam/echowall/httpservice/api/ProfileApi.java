0d4db020629bc80890d6e25c5c5a8810ecc33261
ttong94 <43933188+ttong94@users.noreply.github.com>
2021-03-31 22:33:58 -0400

    git add

--- http-service/src/main/java/com/laiteam/echowall/httpservice/api/ProfileApi.java
+++ http-service/src/main/java/com/laiteam/echowall/httpservice/api/ProfileApi.java
@@ -1,19 +1,22 @@
 package com.laiteam.echowall.httpservice.api;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import com.laiteam.echowall.common.exception.InvalidRequestException;
 import com.laiteam.echowall.dal.entity.Gender;
 import com.laiteam.echowall.dal.entity.Profile;
 import com.laiteam.echowall.dal.entity.User;
 import com.laiteam.echowall.httpservice.response.ProfileResponse;
 import com.laiteam.echowall.service.ProfileService;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 @Controller
@@ -34,13 +37,61 @@ public class ProfileApi {
             "Can't find the current user's profile"));
     }
 
-    @GetMapping(path = "/profile/save")
-    public ResponseEntity<?> saveProfile(@AuthenticationPrincipal User user) {
-        Profile profile = Profile.builder().id(1L).user(User.builder().id(1L).build())
-          .avatarUrl(
-            "https://pa1.narvii.com/6404/35b2929ca438e295554d2460707145d35456f2c2_128.gif")
-          .dob(new Date()).aboutMe("Lazy Man").gender(Gender.FEMALE).build();
-        return ResponseEntity.ok(profileService.saveProfile(profile));
+
+    @RequestMapping(path = "/profile/save", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> saveProfile(@RequestBody ProfileParam profileParam,
+                                         @AuthenticationPrincipal User user) throws Throwable {
+        Optional<Profile> optional = profileService.getProfileById(user.getId());
+        boolean isEmpty = true;
+        if (profileParam.getNickName() != null) {
+            optional.get().setNickName(profileParam.getNickName());
+            isEmpty = false;
+        }
+
+        if (profileParam.getAvatarUrl() != null) {
+            optional.get().setAvatarUrl(profileParam.getAvatarUrl());
+            isEmpty = false;
+        }
+
+        if (profileParam.getGender() != null) {
+            optional.get().setGender(profileParam.getGender());
+            isEmpty = false;
+        }
+
+        if (profileParam.getAboutMe() != null) {
+            optional.get().setAboutMe(profileParam.getAboutMe());
+            isEmpty = false;
+        }
+
+        if (profileParam.getDob() != null) {
+            optional.get().setDob(profileParam.getDob());
+            isEmpty = false;
+        }
+
+        if (profileParam.getResidence() != null) {
+            optional.get().setResidence(profileParam.getResidence());
+            isEmpty = false;
+        }
+
+        if (isEmpty) {
+            throw new InvalidRequestException("No update is posted.");
+        } else {
+            return ResponseEntity.ok(profileService.saveProfile(optional.get()));
+        }
+
+
     }
 
+    @Getter
+    @NoArgsConstructor
+    static class ProfileParam {
+        private Long userId;
+        private String nickName;
+        private String avatarUrl;
+        private Gender gender;
+        private String residence;
+        private Date dob;
+        private String aboutMe;
+    }
 }
